e1b2eed7c31786e5fcb7bdecf34c71b01b2f9a9a
Teddy-XiongGZ <xgz18@mails.tsinghua.edu.cn>
2021-02-10 20:49:21 +0800

    Delete DataETL-dev directory

--- DataETL-dev/src/data_path.py
+++
@@ -1,6 +0,0 @@
-import pathlib
-import os 
-
-REPO_PATH = pathlib.Path(__file__).parent.parent.absolute()
-SRC_PATH = os.path.join(REPO_PATH, "src")
-DATA_PATH = os.path.join(REPO_PATH, "data")
--- DataETL-dev/src/parameters.py
+++
@@ -1,7 +0,0 @@
-class parameters:
-
-    def __init__(self):
-        self.random_seed = 0
-        self.lr = 0.0005
-        self.batch_size = 32
-        self.max_epoch = 40
